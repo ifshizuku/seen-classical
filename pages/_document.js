c4264f5d29423a46b5bf1c9e2e9568add33b9385
Kawasaki Shizuku <starsmoe@outlook.com>
2022-03-11 15:31:40 +0800

    Updating... Add second homepage

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
         <Html lang="zh-cn">
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet" />
             </Head>
             <body>
