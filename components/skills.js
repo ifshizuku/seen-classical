57c715b8eb283586a03320a56c3f5531a1153709
Kawasaki Shizuku <starsmoe@outlook.com>
2022-03-20 15:18:07 +0800

    Complete Skills Cards by Components

--- components/skills.js
+++ components/skills.js
@@ -1,57 +1,96 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { solid, regular, brands } from "@fortawesome/fontawesome-svg-core/import.macro";
-import React, { useState } from "react";
+import skillList from "./skills-data";
+
+// Import Specific Icons
+import { faJsSquare, faReact, faSwift, faSketch } from "@fortawesome/free-brands-svg-icons"
+import { faCompassDrafting, faFilm, faFeatherPointed, faCamera, faPaintbrush, faGuitar} from "@fortawesome/free-solid-svg-icons"
 
 class Skills extends React.Component {
     constructor(props) {
         super(props);
-        this.manage = false;
-        React.createRef();
+        // Hover State Default
         this.state = {
             jshover: false,
+            reacthover: false,
+            swifthover: false,
+            sketchhover: false,
+            designhover: false,
+            videohover: false,
+            writinghover: false,
+            photographyhover: false,
+            painthover: false,
+            guitarhover: false
         };
     }
 
     render() {
-        return (
-            <div id="skills-container" className="flex pt-4">
-                <div id="skills-cards" className="flex">
-                    <div
-                        id="card-js"
-                        className="flex bg-white bg-opacity-20 rounded-md transition hover:bg-opacity-90 hover:bg-yellow-300 hover:scale-110"
-                        onMouseEnter={() => {
-                            this.setState({
-                                jshover: true,
-                            });
-                        }}
-                        onMouseLeave={() => {
-                            this.setState({
-                                jshover: false,
-                            });
-                        }}>
-                        <div className="flex justify-center items-center mt-3 mb-3 ml-5">
-                            <FontAwesomeIcon
-                                size="3x"
-                                icon={brands("js-square")}
-                                color={[this.state.jshover ? "#ffffff" : "#eed94d"]}
-                                className="transition"
-                            />
-                        </div>
-                        <div className="flex flex-col justify-center items-center ml-3 mr-5">
-                            <span className="font-medium text-lg text-white transition">JavaScript</span>
-                            <div
-                                className={[
-                                    "w-full rounded bg-white bg-opacity-20 mt-0.5 flex transition-all",
-                                    this.state.jshover ? ["visible opacity-100 h-2"] : ["invisible opacity-0 h-0"],
-                                ].join(" ")}>
-                                <div className="w-3/4 h-full bg-white bg-opacity-80 rounded transition"></div>
-                            </div>
+        const items = skillList.map((item) => {
+
+            // Icons Dictionary
+            const myIcons = {
+                js: faJsSquare,
+                react: faReact,
+                swift: faSwift,
+                sketch: faSketch,
+                design: faCompassDrafting,
+                video: faFilm,
+                writing: faFeatherPointed,
+                photography: faCamera,
+                paint: faPaintbrush,
+                guitar: faGuitar
+              };
+            //   Finding
+            const fc = item.key;
+
+            return (
+                <div
+                    id={item.id}
+                    className={["flex bg-white bg-opacity-20 rounded-lg transition hover:bg-opacity-90 hover:scale-110 h-16", [item.hover]].join(" ")}
+                    key={item.key}
+                    onMouseEnter={() => {
+                        this.setState({
+                            [item.hoverController]: true,
+                        });
+                    }}
+                    onMouseLeave={() => {
+                        this.setState({
+                            [item.hoverController]: false,
+                        });
+                    }}>
+                    <div className="flex justify-center items-center mt-3.5 mb-3.5 ml-5">
+                        <FontAwesomeIcon
+                            size="2x"
+                            icon={myIcons[fc]}
+                            color={[this.state.[item.hoverController] ? "#ffffff" : item.brandColor]}
+                            className="transition"
+                            id={["icon_" + item.key]}
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center items-center ml-3 mr-5 ">
+                        <span className="font-medium text-lg text-white transition select-none subpixel-antialiased">
+                            {item.fullname}
+                        </span>
+                        <div
+                            className={[
+                                "w-full rounded bg-white bg-opacity-20 mt-0.5 flex transition-all",
+                                this.state.[item.hoverController] ? ["visible opacity-100 h-2"] : ["invisible opacity-0 h-0"],
+                            ].join(" ")}>
+                            <div className={["h-full bg-white bg-opacity-80 rounded transition", item.process].join(" ")}></div>
                         </div>
                     </div>
+                </div>
+            );
+        });
+
+        return (
+            <div id="skills-container" className="flex pt-6">
+                <div id="skills-cards" className="flex flex-wrap gap-6">
+                    {items}
                 </div>
             </div>
         );
     }
+
 }
 
 export default Skills;
