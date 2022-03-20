dbe4f7813f2f4ff03dd52501cb702d787225bfab
Kawasaki Shizuku <starsmoe@outlook.com>
2022-03-20 09:35:53 +0800

    Add Animate to New Card

--- components/skills.js
+++ components/skills.js
@@ -18,23 +18,33 @@ class Skills extends React.Component {
                 <div id="skills-cards" className="flex">
                     <div
                         id="card-js"
-                        className="flex bg-white bg-opacity-20 rounded-md transition"
+                        className="flex bg-white bg-opacity-20 rounded-md transition hover:bg-opacity-90 hover:bg-yellow-300 hover:scale-110"
                         onMouseEnter={() => {
                             this.setState({
                                 jshover: true,
                             });
+                        }}
+                        onMouseLeave={() => {
+                            this.setState({
+                                jshover: false,
+                            });
                         }}>
                         <div className="flex justify-center items-center mt-3 mb-3 ml-5">
-                            <FontAwesomeIcon size="3x" icon={brands("js-square")} color="#eed94d" />
+                            <FontAwesomeIcon
+                                size="3x"
+                                icon={brands("js-square")}
+                                color={[this.state.jshover ? "#ffffff" : "#eed94d"]}
+                                className="transition"
+                            />
                         </div>
                         <div className="flex flex-col justify-center items-center ml-3 mr-5">
-                            <span className="font-medium text-lg text-slate-50 transition">JavaScript</span>
+                            <span className="font-medium text-lg text-white transition">JavaScript</span>
                             <div
                                 className={[
-                                    "w-full h-2 rounded bg-white bg-opacity-20 mt-0.5 flex",
-                                    this.state.jshover ? "flex" : "hidden",
+                                    "w-full rounded bg-white bg-opacity-20 mt-0.5 flex transition-all",
+                                    this.state.jshover ? ["visible opacity-100 h-2"] : ["invisible opacity-0 h-0"],
                                 ].join(" ")}>
-                                <div className="w-3/4 h-full bg-white bg-opacity-80 rounded"></div>
+                                <div className="w-3/4 h-full bg-white bg-opacity-80 rounded transition"></div>
                             </div>
                         </div>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+        color: {
+            transparent: "transparent",
+            current: "currentColor",
+            "js-yellow": "#eed94d",
+        },
+    },
+    plugins: [],
+};
