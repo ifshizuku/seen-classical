b5e50010a9cc521856545d18cc330acd6c9d9ec6
Kawasaki Shizuku <starsmoe@outlook.com>
2022-03-24 21:17:04 +0800

    add site title

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import HomepageLayout from "../components/homepageLayout";
 import React from "react";
 import SubpageProfile from "../components/subpageProfile";
 import Sidebar from "../components/sideBar";
+import Head from "next/head";
 
 class HomePage extends React.Component {
     constructor(props) {
@@ -9,36 +10,43 @@ class HomePage extends React.Component {
         this.state = {
             zenmode: false,
         };
-        this.toggleZen =this.toggleZen.bind(this);
-        this.toggleSidebar =this.toggleSidebar.bind(this);
+        this.toggleZen = this.toggleZen.bind(this);
+        this.toggleSidebar = this.toggleSidebar.bind(this);
     }
 
     toggleZen() {
-        this.setState(state => ({
+        this.setState((state) => ({
             zenmode: state.zenmode === true ? false : true,
         }));
     }
 
     toggleSidebar() {
-        this.setState(state => ({
+        this.setState((state) => ({
             sidebar: state.sidebar === true ? false : true,
         }));
     }
 
     render() {
         return (
-                <div
-                    id="wrap"
-                    className="transition w-full h-full bg-cover bg-center bg-fixed bg-[url('https://seen-1302639736.file.myqcloud.com/site-data/homepage/bg-05.webp')] -z-10">
-                    {/* Content */}
-                    <div id="content" className="flex w-screen justify-center items-center flex-col h-full">
-                        <HomepageLayout zenmode={this.state.zenmode} toggleZen={this.toggleZen} toggleSidebar={this.toggleSidebar}/>
+            <div
+                id="wrap"
+                className="transition w-full h-full bg-cover bg-center bg-fixed bg-[url('https://seen-1302639736.file.myqcloud.com/site-data/homepage/bg-05.webp')] -z-10">
+                <Head>
+                    <title>Seen / 旧念</title>
+                </Head>
+                {/* Content */}
+                <div id="content" className="flex w-screen justify-center items-center flex-col h-full">
+                    <HomepageLayout
+                        zenmode={this.state.zenmode}
+                        toggleZen={this.toggleZen}
+                        toggleSidebar={this.toggleSidebar}
+                    />
 
-                        <SubpageProfile zenmode={this.state.zenmode} />
-                    </div>
-
-                    <Sidebar sidebar={this.state.sidebar} toggleSidebar={this.toggleSidebar}/>
+                    <SubpageProfile zenmode={this.state.zenmode} />
                 </div>
+
+                <Sidebar sidebar={this.state.sidebar} toggleSidebar={this.toggleSidebar} />
+            </div>
         );
     }
 }
