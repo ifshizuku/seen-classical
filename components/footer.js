46f69c36243c6eb3585203dbaa3c453a4a6064c0
Kawasaki Shizuku <starsmoe@outlook.com>
2022-03-28 09:00:24 +0800

    footer struc. init

---
+++ components/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div id="footer" className="flex">
+                <div className="flex items-center justify-center"></div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
