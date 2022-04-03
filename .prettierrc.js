707e8421d9114759847660858943ec627d48df07
Kawasaki Shizuku <starsmoe@outlook.com>
2022-04-03 09:16:04 +0800

    Init

--- .prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    printWidth: 160,
-    bracketSameLine: true,
-    // htmlWhitespaceSensitivity: ignore,
-    tabWidth: 4,
-};
--- components/toConfig.js
+++
@@ -1,5 +0,0 @@
-import configObject from "js-yaml-loader!../config/_config.yml";
-
-const siteConfig = configObject;
-
-export default siteConfig;
